a9f8757c00e5e8883eb87dee435af13308c08084
Laura Messa Travin <laura.travin@gmail.com>
2018-04-10 03:40:50 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,9 @@ def long_planeteer_calls(collection)
 end
 
 def find_the_cheese(collection)
-  cheese_types.map {|name| collection.any?("#{name}") }
+  cheese_types.map {|name| if collection.include?("#{name}")
+   return "#{name}"
+  }
   cheese_types = ["cheddar", "gouda", "camembert"]
 
 end
