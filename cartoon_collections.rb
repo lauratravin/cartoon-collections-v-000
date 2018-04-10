0010a4714ed236221aac987162e29cbf9a72e8b1
Laura Messa Travin <laura.travin@gmail.com>
2018-04-10 03:24:03 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,8 @@ def summon_captain_planet(collection)
   }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(collection)
+  collection.any?{ |name| name.length > 4}
 end
 
 def find_the_cheese# code an argument here
